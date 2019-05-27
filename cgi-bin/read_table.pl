7a5bd0157e1416cee436dfcc7d4d066c9820b5f6
James Gawley <gawley@cchum-kvm-obvil.in2p3.fr>
2019-05-27 12:24:36 +0200

    Force error logging, add bigram_scoring reception from cgi to read_table.

--- cgi-bin/read_table.pl
+++ cgi-bin/read_table.pl
@@ -428,7 +428,8 @@ else {
 	$multi_cutoff    = $query->param('mcutoff')      || $multi_cutoff;
 	@include         = $query->param('include');
 	$recall_cache    = $query->param('recall_cache') || $recall_cache;
-	
+	$bigram_scoring     = $query->param('bigram_scoring')    || $bigram_scoring;
+
 	unless (defined $source) {
 	
 		die "read_table.pl called from web interface with no source";
@@ -438,7 +439,7 @@ else {
 		die "read_table.pl called from web interface with no target";
 	}
 		
-	$quiet = 1;
+	$quiet = 0;
 	
 	# how to redirect browser to results
 
@@ -618,6 +619,7 @@ unless ($quiet) {
 	print STDERR "corpus-wide flag=$corpus_wide\n";
 	print STDERR "File for source frequency=$file_freq_source\n";	
 	print STDERR "File for target frequency=$file_freq_target\n";		
+	print STDERR "Use bigram scoring: $bigram_scoring\n";
 }
 
 
